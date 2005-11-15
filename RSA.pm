e7825153b900a0d2bab5bdb88d449e399e6202ff
Ian Robertson <iroberts+perl@red-bean.com>
2005-11-14 20:34:15 -0800

    import Crypt-OpenSSL-RSA 0.22 from CPAN
    git-cpan-module:   Crypt-OpenSSL-RSA
    git-cpan-version:  0.22
    git-cpan-authorid: IROBERTS
    git-cpan-file:     authors/id/I/IR/IROBERTS/Crypt-OpenSSL-RSA-0.22.tar.gz

--- RSA.pm
+++ RSA.pm
@@ -3,28 +3,16 @@ package Crypt::OpenSSL::RSA;
 use strict;
 use Carp;
 
-use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK $AUTOLOAD
-             $RSA_PKCS1_PADDING $RSA_SSLV23_PADDING $RSA_NO_PADDING
-             $RSA_PKCS1_OAEP_PADDING);
+use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK $AUTOLOAD);
 
-require Exporter;
 require DynaLoader;
 require AutoLoader;
 
-@ISA = qw(Exporter DynaLoader);
-@EXPORT = qw($RSA_PKCS1_PADDING $RSA_SSLV23_PADDING $RSA_NO_PADDING
-             $RSA_PKCS1_OAEP_PADDING);
+@ISA = qw(DynaLoader);
 
 $VERSION = '0.22';
 
 bootstrap Crypt::OpenSSL::RSA $VERSION;
-
-# taken from openssl/rsa.h
-
-$RSA_PKCS1_PADDING = 1;
-$RSA_SSLV23_PADDING = 2;
-$RSA_NO_PADDING = 3;
-$RSA_PKCS1_OAEP_PADDING = 4;
 
 BEGIN { eval { require Crypt::OpenSSL::Bignum; }; }
 
@@ -93,10 +81,19 @@ use_xxx_padding methods
 
 sub new_public_key
 {
-    my ($proto, $p_string) = @_;
-    my $self = $proto->_new();
-    $self->load_public_key($p_string);
-    return $self;
+    my ($proto, $p_key_string) = @_;
+    if ($p_key_string =~ /^-----BEGIN RSA PUBLIC KEY-----/)
+    {
+        return $proto->_new_public_key_pkcs1($p_key_string);
+    }
+    elsif ($p_key_string =~ /^-----BEGIN PUBLIC KEY-----/)
+    {
+        return $proto->_new_public_key_x509($p_key_string);
+    }
+    else
+    {
+        croak "unrecognized key format";
+    }
 }
 
 =item new_private_key
@@ -107,43 +104,13 @@ representation of the key.  The string should include the
 -----BEGIN...----- and -----END...----- lines.  The padding is set to
 PKCS1_OAEP, but can be changed with use_xxx_padding.
 
-=cut
-
-sub new_private_key
-{
-    my $self = shift->_new();
-    $self->load_private_key(@_);
-    return $self;
-}
-
 =item generate_key
 
 Create a new Crypt::OpenSSL::RSA object by constructing a
 private/public key pair.  The first (mandetory) argument is the key
 size, while the second optional argument specifies the public exponent
-(the default public exponent is 65535).  The padding is set to
+(the default public exponent is 65537).  The padding is set to
 PKCS1_OAEP, but can be changed with use_xxx_padding methods.
-
-I<NOTE> - using generate_key as an instance method on an rsa object
-  created by the deprecated new method is itself deprecated.
-
-=cut
-
-sub generate_key
-{
-    if (ref $_[0])
-    {
-        warn "use of generate_key as an instance method instead of as a constructor has been deprecated.";
-        my $self = shift;
-        $self->_generate_key(@_);
-    }
-    else
-    {
-        my $self = shift->_new();
-        $self->_generate_key(@_);
-        return $self;
-    }
-}
 
 =item new_key_from_parameters
 
@@ -180,29 +147,6 @@ sub import_random_seed
     }
 }
 
-=item new
-
-The no-arg new constructor is I<DEPRECATED> - use generate_key,
-new_public_key or new_private_key instead.
-
-=cut
-
-sub new
-{
-    warn "Crypt::OpenSSL::RSA::new is deprecated";
-    return shift->_new();
-}
-
-#deprecated
-
-sub _new
-{
-    my $self = bless {}, shift;
-    $self->use_pkcs1_oaep_padding();
-    $self->use_sha1_hash();
-    return $self;
-}
-
 =back
 
 =head1 Instance Methods
@@ -213,40 +157,6 @@ sub _new
 
 Clean up after ourselves.  In particular, erase and free the memory
 occupied by the RSA key structure.
-
-=cut
-
-sub DESTROY
-{
-    shift->_free_RSA_key();
-}
-
-=item load_public_key
-
-I<DEPRECATED> - use new_public_key instead
-
-=cut
-
-sub load_public_key
-{
-    my ($self, $p_key_string) = @_;
-    if ($p_key_string =~ /^-----BEGIN RSA PUBLIC KEY-----/)
-    {
-        $self->_load_public_pkcs1_key($p_key_string);
-    }
-    elsif ($p_key_string =~ /^-----BEGIN PUBLIC KEY-----/)
-    {
-        $self->_load_public_x509_key($p_key_string);
-    }
-    else
-    {
-        croak "unrecognized key format";
-    }
-}
-
-=item load_private_key
-
-I<DEPRECATED> - use new_private_key instead
 
 =item get_public_key_string
 
@@ -274,37 +184,28 @@ Return the DER-encoded PKCS1 representation of the private key.
 
 =item encrypt
 
-Encrypt a string using the public (portion of the) key
-
-=item sign
-
-Sign a string using the secret (portion of the) key
-
-=item verify
-
-Check the signature on a text.
+Encrypt a binary "string" using the public (portion of the) key.
 
 =item decrypt
 
 Decrypt a binary "string".  Croaks if the key is public only.
 
-=item set_padding_mode
+=item private_encrypt
 
-I<DEPRECATED>  Use the use_xxx_padding methods instead
+Encrypt a binary "string" using the private key.  Croaks if the key is
+public only.
 
-=cut
+=item public_decrypt
 
-sub set_padding_mode
-{
-    my $self = shift;
-    warn "set_padding_mode is deprecated.  Use use_xxx_padding instead";
-    shift->_set_padding_mode(@_) ;
-}
+Decrypt a binary "string" using the public (portion of the) key.
 
-sub _set_padding_mode
-{
-    $_[0]->{_Padding_Mode} = $_[1];
-}
+=item sign
+
+Sign a string using the secret (portion of the) key.
+
+=item verify
+
+Check the signature on a text.
 
 =item use_no_padding
 
@@ -328,25 +229,6 @@ Crypt::OpenSSL::RSA.
 
 Use PKCS #1 v1.5 padding with an SSL-specific modification that
 denotes that the server is SSL3 capable.
-
-=cut
-
-sub use_sslv23_padding
-{
-    shift->_set_padding_mode($RSA_SSLV23_PADDING);
-}
-
-=item get_padding_mode
-
-I<DEPRECATED>
-
-=cut
-
-sub get_padding_mode
-{
-    warn "get_padding_mode is depreceated";
-    return shift->{_Padding_Mode};
-}
 
 =item use_md5_hash
 
--- t/rsa.t
+++ t/rsa.t
@@ -4,18 +4,39 @@ use Test;
 use Crypt::OpenSSL::Random;
 use Crypt::OpenSSL::RSA;
 
-BEGIN { plan tests => 19 }
+BEGIN { plan tests => 31 }
+
+sub _Test_Encrypt_And_Decrypt
+{
+    my ($p_plaintext_length, $p_rsa, $p_check_private_encrypt) = @_;
+
+    my ($ciphertext, $decoded_text);
+    my $plaintext = pack("C${p_plaintext_length}",
+                         (1,255,0,128,4, # Make sure these characters work
+                          map {int(rand 256)} (1 .. $p_plaintext_length - 5)));
+    ok($ciphertext = $p_rsa->encrypt($plaintext));
+    ok($decoded_text = $p_rsa->decrypt($ciphertext));
+    ok($decoded_text eq $plaintext);
+
+    if ($p_check_private_encrypt)
+    {
+        ok($ciphertext = $p_rsa->private_encrypt($plaintext));
+        ok($decoded_text = $p_rsa->public_decrypt($ciphertext));
+        ok($decoded_text eq $plaintext);
+    }
+}
 
 sub _Test_Sign_And_Verify
 {
-    my( $plaintext, $rsa, $rsa_pub ) = @_;
+    my ($plaintext, $rsa, $rsa_pub) = @_;
 
     my $sig = $rsa->sign($plaintext);
-    ok( $rsa_pub->verify( $plaintext, $sig ) );
+    ok($rsa_pub->verify($plaintext, $sig));
 
     my $false_sig = unpack "H*", $sig;
     $false_sig =~ tr/[a-f]/[0a-d]/;
-    ok(! $rsa_pub->verify( $plaintext, pack( "H*", $false_sig ) ) );
+    ok(! $rsa_pub->verify($plaintext, pack("H*", $false_sig)));
+    ok(! $rsa->verify($plaintext, pack("H*", $false_sig)));
 }
 
 # On platforms without a /dev/random, we need to manually seed.  In
@@ -29,65 +50,56 @@ sub _Test_Sign_And_Verify
 Crypt::OpenSSL::Random::random_seed("Here are 20 bytes...");
 Crypt::OpenSSL::RSA->import_random_seed();
 
-ok( Crypt::OpenSSL::RSA->generate_key( 512 )->size() * 8 == 512 );
+ok(Crypt::OpenSSL::RSA->generate_key(512)->size() * 8 == 512);
 
-my $rsa = Crypt::OpenSSL::RSA->generate_key( 1024 );
-ok( $rsa->size() * 8 == 1024 );
-ok( $rsa->check_key() );
+my $rsa = Crypt::OpenSSL::RSA->generate_key(1024);
+ok($rsa->size() * 8 == 1024);
+ok($rsa->check_key());
 
-# check if NO_PADDING works
 $rsa->use_no_padding();
-my ($ciphertext, $decoded_text);
-my $plaintext = "X" x $rsa->size;
-ok($ciphertext = $rsa->encrypt($plaintext));
-ok($decoded_text = $rsa->decrypt($ciphertext));
-ok ($decoded_text eq $plaintext);
+_Test_Encrypt_And_Decrypt($rsa->size(), $rsa, 1);
 
-# check if OAEP_PADDING works
-my $plaintext_length = $rsa->size() - 42;
-$plaintext = pack("C$plaintext_length",
-                     (255,0,128,4, # Make sure these characters work
-                      map {int(rand 256)} (1..$plaintext_length-4)));
+$rsa->use_pkcs1_padding();
+_Test_Encrypt_And_Decrypt($rsa->size() - 11, $rsa, 1);
+
 $rsa->use_pkcs1_oaep_padding();
-ok($ciphertext = $rsa->encrypt($plaintext));
-ok($decoded_text = $rsa->decrypt($ciphertext));
-ok ($decoded_text eq $plaintext);
+# private_encrypt does not work with pkcs1_oaep_padding
+_Test_Encrypt_And_Decrypt($rsa->size() - 42, $rsa, 0);
+
+#FIXME - use_sslv23_padding seems to fail on decryption.  openssl bug?
 
 my $private_key_string = $rsa->get_private_key_string();
 my $public_key_string = $rsa->get_public_key_string();
 
-ok( $private_key_string and $public_key_string );
+ok($private_key_string and $public_key_string);
 
+my $plaintext = "The quick brown fox jumped over the lazy dog";
 my $rsa_priv = Crypt::OpenSSL::RSA->new_private_key($private_key_string);
-$decoded_text = $rsa_priv->decrypt($ciphertext);
+ok($plaintext eq $rsa_priv->decrypt($rsa_priv->encrypt($plaintext)));
 
-ok( $decoded_text eq $plaintext );
-
-my $rsa_pub = Crypt::OpenSSL::RSA->new_public_key( $public_key_string );
-
-$ciphertext = $rsa_pub->encrypt($plaintext);
-$decoded_text = $rsa->decrypt($ciphertext);
-ok ($decoded_text eq $plaintext);
+my $rsa_pub = Crypt::OpenSSL::RSA->new_public_key($public_key_string);
+$rsa->use_pkcs1_oaep_padding();
+ok($plaintext eq $rsa->decrypt($rsa_pub->encrypt($plaintext)));
 
 $plaintext .= $plaintext x 5;
 # check signature algorithms
 $rsa->use_md5_hash();
 $rsa_pub->use_md5_hash();
-_Test_Sign_And_Verify( $plaintext, $rsa, $rsa_pub );
+_Test_Sign_And_Verify($plaintext, $rsa, $rsa_pub);
 
 $rsa->use_sha1_hash();
 $rsa_pub->use_sha1_hash();
-_Test_Sign_And_Verify( $plaintext, $rsa, $rsa_pub );
+_Test_Sign_And_Verify($plaintext, $rsa, $rsa_pub);
 
 $rsa->use_ripemd160_hash();
 $rsa_pub->use_ripemd160_hash();
-_Test_Sign_And_Verify( $plaintext, $rsa, $rsa_pub );
+_Test_Sign_And_Verify($plaintext, $rsa, $rsa_pub);
 
 # check subclassing
 
-eval { Crypt::OpenSSL::RSA::Subpackage->generate_key( 256 ); };
-ok( !$@ );
+eval { Crypt::OpenSSL::RSA::Subpackage->generate_key(256); };
+ok(!$@);
 
 package Crypt::OpenSSL::RSA::Subpackage;
 
-use base qw( Crypt::OpenSSL::RSA );
+use base qw(Crypt::OpenSSL::RSA);
